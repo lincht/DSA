eab07b1af05bfd065109616a6a75bd7f2d30250b
lincht <lincht@users.noreply.github.com>
2019-01-27 23:05:02 +0900

    Add queue

--- ch3/linear_data_structure.py
+++ ch3/linear_data_structure.py
@@ -18,3 +18,36 @@ class Stack:
     
     def size(self):
         return len(self.items)
+
+
+class Queue:
+    """Implementation of the queue abstract data type where the rear is at
+    position 0 of the list.
+    """
+    
+    def __init__(self):
+        self.items = []
+    
+    def is_empty(self):
+        return self.items == []
+    
+    def enqueue(self, item):
+        self.items.insert(0, item)
+    
+    def dequeue(self):
+        return self.items.pop()
+    
+    def size(self):
+        return len(self.items)
+
+
+class EndRearQueue(Queue):
+    """Implementation of the queue abstract data type where the rear is at
+    the end of the list.
+    """
+    
+    def enqueue(self, item):
+        self.items.append(item)
+    
+    def dequeue(self):
+        return self.items.pop(0)
